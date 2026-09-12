13d6a094b95052d6dc8719173b59237136b5b72b
Animesh Karmakar <animeshkarmakar710@gmail.com>
2026-09-12 23:07:57 +0530

    refactor: expose IMD current weather connector

--- services/ingestion/weathergpt_ingestion/connectors/__init__.py
+++ services/ingestion/weathergpt_ingestion/connectors/__init__.py
@@ -1,6 +1,11 @@
-from .imd import ImdBulletinConnector
+from .imd import ImdCurrentWeatherConnector
 from .noaa import NoaaForecastConnector
 from .open_meteo import OpenMeteoConnector
 from .wis2_mqtt import Wis2MqttSubscriber
 
-__all__ = ["ImdBulletinConnector", "NoaaForecastConnector", "OpenMeteoConnector", "Wis2MqttSubscriber"]
+__all__ = [
+    "ImdCurrentWeatherConnector",
+    "NoaaForecastConnector",
+    "OpenMeteoConnector",
+    "Wis2MqttSubscriber",
+]
